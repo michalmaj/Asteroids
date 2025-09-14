bd1ca0edf51b5b455ba818c325ccf4430b899e40
Michał Maj <michalpmaj@gmail.com>
2025-09-14 07:43:04 +0200

    Limit framerate

--- main.py
+++ main.py
@@ -5,17 +5,28 @@ import pygame
 from constants import *
 
 def main():
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-
+    # Create new window with given width and height
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Create clock and delta time
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Main game loop
     while True:
+        # Check if window is closed, if so, quit the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Fill window with black color
         screen.fill((0, 0, 0), rect=None, special_flags=0)
+        
+        # Constantly refresh window
         pygame.display.flip()
+
+        # Limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
