17fd950267429dd2785c2c5f9bb24f9e62b505da
Michał Maj <michalpmaj@gmail.com>
2025-09-14 11:30:33 +0200

    Add splitting asteroids during taking a shot

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         # Constantly refresh window
         pygame.display.flip()
